e20be89508c1c838395bdef5bbbbc78d04365fbb
Yorinton <ka2ki.yori@outlook.com>
2022-02-05 23:58:01 +0900

    wasm tutorial

---
+++ site/index.js
@@ -0,0 +1,5 @@
+const js = import('../pkg/wasm_practice_bg.js');
+
+js.then((js) => {
+    js.greet('World');
+});
---
+++ site/webpack.config.js
@@ -0,0 +1,20 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+    },
+    mode: 'development',
+    experiments: {
+        syncWebAssembly: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin()
+    ],
+    devServer: {
+
+    }
+};
